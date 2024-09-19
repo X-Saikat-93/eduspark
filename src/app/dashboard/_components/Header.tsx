b7cf4831efb781ee503f4f4973758a314715b1cf
Saikat Das <dassaikat1901@gmail.com>
2024-09-19 20:32:59 +0530

    Refactor Database Schema

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -4,8 +4,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-5 shadow-sm">
-      <Image src={"/logo.png"} alt="logo" width={150} height={100} priority className="object-cover" />
+    <div className='flex justify-between items-center p-5 shadow-sm'>
+      <Image
+        src='/mainlogoscale.png'
+        alt='logo'
+        width={150}
+        height={100}
+        priority
+        className='object-cover'
+      />
       <UserButton />
     </div>
   );
