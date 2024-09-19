9c36f9b15db41adf065df5a0905f9136c0feb95c
Saikat Das <dassaikat1901@gmail.com>
2024-09-19 19:29:53 +0530

    update header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,22 +9,32 @@ import { ModeToggle } from "@/components/ModeToggle";
 
 const Header = () => {
   const { user } = useUser();
+
   return (
-    <div className="flex justify-between p-5 shadow-sm">
-      <Image src={"/mainlogoscale.png"} alt="logo" width={150} height={100} priority className="object-cover" />
-      <div>
+    <header className='flex items-center justify-between  p-4  border-b-2 top-0 z-50 sticky w-full border-black dark:border-slate-200 shadow-lg lg:shadow-xl shadow-slate-700/5 lg:backdrop-blur-sm backdrop-blur-lg lg:after:hidden rounded-xl dark:bg-inherit'>
+      <Link href='/'>
+        <Image
+          src='/mainlogoscale.png'
+          alt='logo'
+          width={150}
+          height={100}
+          priority
+          className='object-cover'
+        />
+      </Link>
+      <div className='flex items-center space-x-4'>
         <ModeToggle />
+        {!user ? (
+          <Link href='/sign-up'>
+            <ShinyButton text='Sign Up' />
+          </Link>
+        ) : (
+          <Link href='/dashboard'>
+            <Button>Dashboard</Button>
+          </Link>
+        )}
       </div>
-      {!user ? (
-        <Link href="/sign-up">
-          <ShinyButton text="Sign Up" />
-        </Link>
-      ) : (
-        <Link href="/dashboard">
-          <Button>Dashboard</Button>
-        </Link>
-      )}
-    </div>
+    </header>
   );
 };
 
