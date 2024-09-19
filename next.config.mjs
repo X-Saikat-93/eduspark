87aa8ba549cb2700b10138c67e5bc910d3d0168c
Saikat Das <dassaikat1901@gmail.com>
2024-09-19 12:52:13 +0530

    Refactor Auth Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "127.0.0.1",
+      "images.unsplash.com",
+      "unsplash.com",
+      "plus.unsplash.com",
+      "firebasestorage.googleapis.com",
+      "img.clerk.com",
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import DropdownMenuRadioGroupDemo from "@/components/dropdown-check";
-import { Button } from "@/components/ui/button";
 import Header from "./_components/Header";
 export default function Home() {
   return (
     <>
 
-      {/* <div className=" flex w-full justify-between">
-
-        <Button>
-          check
-        </Button>
-
-        <DropdownMenuRadioGroupDemo/>
-</div> */}
+     
       <div>
         <Header/>
       </div>
