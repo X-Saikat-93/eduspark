90cd67697627927b6c6f31c8854b094975c4651f
Saikat Das <dassaikat1901@gmail.com>
2024-09-19 05:32:33 +0530

    Setup next theme for toggle mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
     <>
 
-      <Button>
-      hello
-      </Button>
+      <div className=" flex w-full justify-between">
+
+        <Button>
+          check
+        </Button>
+
+        <ModeToggle/>
+</div>
     </>
   );
 }
