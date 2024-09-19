30a01403952f8df75de9400fe5cc0fe69f2d0b49
Saikat Das <dassaikat1901@gmail.com>
2024-09-19 08:28:04 +0530

    setup clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import DropdownMenuRadioGroupDemo from "@/components/dropdown-check";
 import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
 export default function Home() {
   return (
     <>
 
-      <div className=" flex w-full justify-between">
+      {/* <div className=" flex w-full justify-between">
 
         <Button>
           check
         </Button>
 
         <DropdownMenuRadioGroupDemo/>
-</div>
+</div> */}
+      <div>
+        <Header/>
+      </div>
     </>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+export function ny(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
